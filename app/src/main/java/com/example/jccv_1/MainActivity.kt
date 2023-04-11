6c0d76eaca2283fcc3e0a710f5b03461b928b155
atinaree <christian_cv.90@hotmail.com>
2023-04-11 13:41:28 +0200

    modificado dependencias, creado retrofit

--- app/src/main/java/com/example/jccv_1/MainActivity.kt
+++ app/src/main/java/com/example/jccv_1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.jccv_1.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +16,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
-
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://viewnextandroid.mocklab.io/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
 
     }
+
 }
