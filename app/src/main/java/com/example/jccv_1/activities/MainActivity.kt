aa0a235059b910460360ecfac6c560688782255d
atinaree <christian_cv.90@hotmail.com>
2023-04-24 13:12:22 +0200

    Creados los intents para pasar la informacion de los filtros del secondary al main, peleandome fuertemente con las corutinas

--- app/src/main/java/com/example/jccv_1/activities/MainActivity.kt
+++ app/src/main/java/com/example/jccv_1/activities/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.jccv_1.activities
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.*
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jccv_1.R
+import com.example.jccv_1.activities.SecondaryActivity.Companion.anuladas
+import com.example.jccv_1.activities.SecondaryActivity.Companion.cfija
+
+import com.example.jccv_1.activities.SecondaryActivity.Companion.fecha
+import com.example.jccv_1.activities.SecondaryActivity.Companion.fecha2
+import com.example.jccv_1.activities.SecondaryActivity.Companion.importe
+import com.example.jccv_1.activities.SecondaryActivity.Companion.pagadas
+import com.example.jccv_1.activities.SecondaryActivity.Companion.pendientes
+import com.example.jccv_1.activities.SecondaryActivity.Companion.plan
 import com.example.jccv_1.red.RetrofitToRoom
 import com.example.jccv_1.databinding.ActivityMainBinding
 import com.example.jccv_1.modeladoDatos.CustomAdapter
@@ -14,52 +28,68 @@ import com.example.jccv_1.modeladoDatos.Facturas
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.jar.Attributes.Name
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val adapter = CustomAdapter()
-    private var currentList = ArrayList<Facturas>()
+    private val secondaryLauncher = registerForActivityResult(StartActivityForResult()){ activityResult ->
+        if(activityResult.resultCode == RESULT_OK){
 
+            val fecha1 = activityResult.data?.getStringExtra(fecha).orEmpty()
+            val fecha2 = activityResult.data?.getStringExtra(fecha2).orEmpty()
+            val importe = activityResult.data?.getStringExtra(importe).orEmpty()
+            val pagadas = activityResult.data?.getStringExtra(pagadas).orEmpty()
+            val anuladas = activityResult.data?.getStringExtra(anuladas).orEmpty()
+            val cfija = activityResult.data?.getStringExtra(cfija).orEmpty()
+            val pendientes = activityResult.data?.getStringExtra(pendientes).orEmpty()
+            val plan = activityResult.data?.getStringExtra(plan).orEmpty()
+
+
+
+
+            Toast.makeText(this, pagadas, Toast.LENGTH_SHORT).show()
+
+    Log.d("hii",pagadas)
+
+
+
+        }else{
+            Toast.makeText(this, "Result not OK", Toast.LENGTH_SHORT).show()
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val retrofitToRoom = RetrofitToRoom(application)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.buttonFilter.setOnClickListener {
-            val intent = Intent(this, SecondaryActivity::class.java)
-            startActivity(intent)
-        }
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
-
-
         lifecycleScope.launch {
             val myDataList = withContext(Dispatchers.IO) {
                 retrofitToRoom.getMyData()
             }
             adapter.setData(myDataList as ArrayList<Facturas>)
         }
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val retrofitToRoom = RetrofitToRoom(application)
-        lifecycleScope.launch {
-            val newMyDataList = withContext(Dispatchers.IO) {
-                retrofitToRoom.getMyData()
-
-            }
-
-            newMyDataList.filter { facturas: Facturas -> facturas.descEstado == "Pagada" }
-
-            adapter.setData(newMyDataList as ArrayList<Facturas>)
-            Log.d("jirafa",newMyDataList.toString())
-            currentList = newMyDataList as ArrayList<Facturas>
+        binding.buttonFilter.setOnClickListener {
+            val intent = Intent(this, SecondaryActivity::class.java)
+            secondaryLauncher.launch(intent)
         }
     }
+
+
+
+
+
 }
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/jccv_1/activities/SecondaryActivity.kt
+++ app/src/main/java/com/example/jccv_1/activities/SecondaryActivity.kt
@@ -1,7 +1,7 @@
 package com.example.jccv_1.activities
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.*
 import com.example.jccv_1.R
@@ -11,19 +11,28 @@ import com.example.jccv_1.databinding.SecondaryActivityBinding
 import com.example.jccv_1.modeladoDatos.CustomAdapter
 import com.example.jccv_1.modeladoDatos.Facturas
 import com.example.jccv_1.secondary.DatePickerManager
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.*
-import kotlin.collections.ArrayList
-
 class SecondaryActivity : Activity() {
     lateinit var binding: SecondaryActivityBinding
     lateinit var itemfiltross: LinearLayout
     lateinit var botonaplicar: Button
     lateinit var adapter: CustomAdapter
     val dataDao: facturaDAO = facturasAPP.room.facturaDAO()
-    var filteredDataList: ArrayList<Facturas> = ArrayList()
+    companion object{
+        const val fecha = "fechaini"
+        const val fecha2 = "fechafin"
+        const val importe = "importe"
+        const val pagadas = "pagadas"
+        const val anuladas = "anuladas"
+        const val cfija = "cfija"
+        const val pendientes = "pendientes"
+        const val plan = "plan"
 
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Configuracion regional
@@ -32,11 +41,9 @@ class SecondaryActivity : Activity() {
         val config = baseContext.resources.configuration
         config.setLocale(locale)
         baseContext.createConfigurationContext(config)
-
         super.onCreate(savedInstanceState)
         binding = SecondaryActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         // Cargar layout en el LinearLayout
         val inflater = LayoutInflater.from(this)
         itemfiltross = binding.itemfiltross
@@ -46,13 +53,10 @@ class SecondaryActivity : Activity() {
         val button2 = item.findViewById<Button>(R.id.botonFechaFin)
         DatePickerManager(button1)
         DatePickerManager(button2)
-
         // Crear un DatePickerManager para el botón 1
         val datePickerManager1 = DatePickerManager(button1)
         // Crear un DatePickerManager para el botón 2
         val datePickerManager2 = DatePickerManager(button2)
-
-
 // Establecer la fecha mínima para el DatePicker del botón 2 como la fecha seleccionada en el botón 1
         button2.setOnClickListener {
             val minDate = datePickerManager1.getDate() // Obtener la fecha seleccionada en el botón 1
@@ -60,48 +64,78 @@ class SecondaryActivity : Activity() {
                 datePickerManager2.setMinDate(minDate) // Establecer la fecha mínima para el DatePicker del botón 2
             }
         }
-
-
         //Boton para cerrar la vista
         binding.buttonFilter.setOnClickListener {
             finish()
-
         }
-
         // Boton para quitar filtros
         binding.button3.setOnClickListener {
             button1.text = "dia/mes/año"
             button2.text = "dia/mes/año"
-
             val slider = findViewById<SeekBar>(R.id.barraImporte)
             val checkBox1 = findViewById<CheckBox>(R.id.chPagada)
             val checkBox2 = findViewById<CheckBox>(R.id.chAnulada)
             val checkBox3 = findViewById<CheckBox>(R.id.chCuotaFija)
             val checkBox4 = findViewById<CheckBox>(R.id.chPendientes)
             val checkBox5 = findViewById<CheckBox>(R.id.chPlan)
-
             slider.progress = 0
             checkBox1.isChecked = false
             checkBox2.isChecked = false
             checkBox3.isChecked = false
             checkBox4.isChecked = false
             checkBox5.isChecked = false
-
         }
         //Boton aplicar
         botonaplicar = binding.button
         adapter = CustomAdapter()
-        botonaplicar.setOnClickListener {
-            suspend {
-                coroutineScope {
-                    launch {
-                        dataDao.getALL().filter { facturas: Facturas -> facturas.descEstado == "Pagadas"  }
 
-                    }
-                }
-            }
+        botonaplicar.setOnClickListener {
+                       aplicarFiltros()
+
+
+
+
+        }
+
+    }
+     suspend fun aplicarFiltros() {
+
+        withContext(Dispatchers.IO) {
+            val slider = findViewById<SeekBar>(R.id.barraImporte)
+            val checkBox1 = findViewById<CheckBox>(R.id.chPagada)
+            val checkBox2 = findViewById<CheckBox>(R.id.chAnulada)
+            val checkBox3 = findViewById<CheckBox>(R.id.chCuotaFija)
+            val checkBox4 = findViewById<CheckBox>(R.id.chPendientes)
+            val checkBox5 = findViewById<CheckBox>(R.id.chPlan)
+            val intent = Intent()
+
+            var filtroPagadas = dataDao.getALL().filter { facturas: Facturas -> facturas.descEstado == "Pagadas" }
+            var filtroAnuladas = dataDao.getALL().filter { facturas: Facturas -> facturas.descEstado == "Anuladas" }
+            var filtroCfija = dataDao.getALL().filter { facturas: Facturas -> facturas.descEstado == "Cuota Fija" }
+            var filtroPendientes = dataDao.getALL().filter { facturas: Facturas -> facturas.descEstado == "Pendientes de pago" }
+            var filtroPlan = dataDao.getALL().filter { facturas: Facturas -> facturas.descEstado == "Plan de pago" }
+
+
+            intent.putExtra(fecha, "pepitoo")
+            intent.putExtra(fecha2, "2")
+            intent.putExtra(importe, "3")
+            if (checkBox1.isChecked)
+                intent.putExtra(pagadas, filtroPagadas.toString())
+            if (checkBox2.isChecked)
+                intent.putExtra(anuladas, filtroAnuladas.toString())
+            if (checkBox3.isChecked)
+                intent.putExtra(cfija, filtroCfija.toString())
+            if (checkBox4.isChecked)
+                intent.putExtra(pendientes, filtroPendientes.toString())
+            if (checkBox5.isChecked)
+                intent.putExtra(plan, filtroPlan.toString())
+
+
+            setResult(RESULT_OK, intent)
+            finish()
         }
     }
+
 }
 
 
