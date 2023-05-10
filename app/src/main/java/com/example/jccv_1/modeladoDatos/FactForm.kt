de075f0c476256a85cef7a38cdc605e4ce603998
atinaree <christian_cv.90@hotmail.com>
2023-05-10 09:44:11 +0200

    FactForm y Facturas organizado, limpio y comentado

--- app/src/main/java/com/example/jccv_1/modeladoDatos/FactForm.kt
+++ app/src/main/java/com/example/jccv_1/modeladoDatos/FactForm.kt
@@ -1,4 +1,5 @@
 package com.example.jccv_1.modeladoDatos
+// Clase de datos con la estructura del jSon
 data class FactForm (
     val numFacturas: Int,
     val facturas: List<Facturas>
--- app/src/main/java/com/example/jccv_1/modeladoDatos/Facturas.kt
+++ app/src/main/java/com/example/jccv_1/modeladoDatos/Facturas.kt
@@ -1,10 +1,21 @@
 package com.example.jccv_1.modeladoDatos
+
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-@Entity (tableName = "facturas")
+
+// Anotación @Entity indica que esta clase representa una entidad de la base de datos
+@Entity(tableName = "facturas")
 data class Facturas(
+    // Anotación @PrimaryKey indica que el campo "id" es la clave primaria de la entidad
     @PrimaryKey(autoGenerate = true)
     val id: Int,
+
+    // Campo que almacena la descripción del estado de la factura
     val descEstado: String,
+
+    // Campo que almacena el importe de la factura
     val importeOrdenacion: Double,
-    val fecha: String)
+
+    // Campo que almacena la fecha de la factura
+    val fecha: String
+)
