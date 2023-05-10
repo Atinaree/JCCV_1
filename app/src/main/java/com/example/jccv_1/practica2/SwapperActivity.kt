d7d87644a478a23786f9362705dca9528a1f955a
atinaree <christian_cv.90@hotmail.com>
2023-05-10 10:40:30 +0200

    Clase Swapper y ViewPager de la practica 2 organizada comentada

--- app/src/main/java/com/example/jccv_1/practica2/SwapperActivity.kt
+++ app/src/main/java/com/example/jccv_1/practica2/SwapperActivity.kt
@@ -9,32 +9,32 @@ import com.example.jccv_1.red.RetrofitToRoom
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-
 class SwapperActivity: AppCompatActivity() {
     private lateinit var binding: SwapperBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = SwapperBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configurar el click listener para el botón con ID button5
         binding.button5.setOnClickListener() {
-                val intent = Intent(applicationContext, MainActivity::class.java)
-                startActivity(intent)
-            }
-
-        binding.button6.setOnClickListener(){
-                val intent = Intent(applicationContext, Actividad2::class.java)
-                startActivity(intent)
-
+            // Crear un intent para abrir la actividad MainActivity
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            // Iniciar la actividad MainActivity
+            startActivity(intent)
         }
 
+        // Lo mismo que el de arriba pero para otro activity
+        binding.button6.setOnClickListener(){
+            val intent = Intent(applicationContext, Actividad2::class.java)
+            startActivity(intent)
+        }
+
+        // Cargamos datos en la base de datos.
         GlobalScope.launch {
             val retrofitToRoom = RetrofitToRoom(application)
             retrofitToRoom.getMyData()
         }
-
     }
-
 }
--- app/src/main/java/com/example/jccv_1/practica2/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/jccv_1/practica2/ViewPagerAdapter.kt
@@ -4,21 +4,25 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
+// Clase ViewPagerAdapter que extiende de FragmentPagerAdapter
 class ViewPagerAdapter(fm: FragmentManager, var tabCount: Int) : FragmentPagerAdapter(fm) {
 
+    // Metodo getItem que devuelve el fragmento correspondiente a una posicion dada
     override fun getItem(position: Int): Fragment {
         return when (position) {
-            0 -> FirstFragment()
-            1 -> SecondFragment()
-            2 -> ThirdFragment()
-            else -> FirstFragment()
+            0 -> FirstFragment()  // Fragmento para la posición 0
+            1 -> SecondFragment() // Fragmento para la posición 1
+            2 -> ThirdFragment()  // Fragmento para la posición 2
+            else -> FirstFragment() // Fragmento predeterminado, en caso de posición inválida
         }
     }
 
+    // Metodo getCount que devuelve el numero total de fragmentos/tabulaciones
     override fun getCount(): Int {
         return tabCount
     }
 
+    // Metodo getPageTitle que devuelve el título de la pagina/tabulacion en la posicion dada
     override fun getPageTitle(position: Int): CharSequence {
         return "Tab " + (position + 1)
     }
