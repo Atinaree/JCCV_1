c3b7daf99dea9ec61f57287eefaa39a624830781
atinaree <christian_cv.90@hotmail.com>
2023-05-10 09:38:08 +0200

    Revisado codigo de base de datos (DAO, app y DB) organizado y comentado

--- app/src/main/java/com/example/jccv_1/database/FacturasDB.kt
+++ app/src/main/java/com/example/jccv_1/database/FacturasDB.kt
@@ -1,11 +1,14 @@
 package com.example.jccv_1.database
+
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.jccv_1.modeladoDatos.Facturas
+
+// Definición de la base de datos utilizando la anotación @Database
 @Database(
-    entities = [Facturas::class],
-    version = 1
+    entities = [Facturas::class], // Especifica las entidades de la base de datos (en este caso, solo Facturas)
+    version = 1 // Especifica la versión de la base de datos
 )
 abstract class FacturasDB : RoomDatabase() {
-    abstract fun facturaDAO(): facturaDAO
+    abstract fun facturaDAO(): facturaDAO // Declara el método abstracto para obtener el DAO (Data Access Object) de Facturas
 }
--- app/src/main/java/com/example/jccv_1/database/facturasAPP.kt
+++ app/src/main/java/com/example/jccv_1/database/facturasAPP.kt
@@ -1,12 +1,17 @@
 package com.example.jccv_1.database
+
 import android.app.Application
 import androidx.room.Room
+
 class facturasAPP : Application() {
     companion object {
         lateinit var room: FacturasDB
     }
+
     override fun onCreate() {
         super.onCreate()
+
+        // Crea una instancia de la base de datos usando ROOM
         room = Room.databaseBuilder(applicationContext, FacturasDB::class.java, "facturas")
             .fallbackToDestructiveMigration()
             .build()
